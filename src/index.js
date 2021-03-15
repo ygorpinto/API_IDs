656979aa94e695a0f4cea3008a1b5ed6ab4fc241
Ygor Ferreira <ygor.pinto@hotmail.com>
2021-03-15 17:38:38 -0300

    ok'

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoDBConnect = require('./database/index')
+const cors = require('cors')
 
 const app = express();
-
+app.use(cors());
 app.use(express.json({ extended : false }));
 app.use('/api',require('./controllers/index'));
 
